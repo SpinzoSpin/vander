43d47fd372a1b2eac09f2c2197e7f54aaf5a2e6f
azzaky9 <zakiiws22@gmail.com>
2026-05-12 00:42:23 +0700

    feat: initialize Prisma ORM and NextAuth configuration for user authentication

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "../generated/prisma/client"
+import { withAccelerate } from "@prisma/extension-accelerate"
+
+const prismaClientFactory = () => {
+    return new PrismaClient().$extends(withAccelerate())
+}
+
+type PrismaClientExtended = ReturnType<typeof prismaClientFactory>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientExtended | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientFactory()
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
